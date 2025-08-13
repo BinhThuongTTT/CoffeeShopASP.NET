f01a675784c24e52ef4479621fa6371c0dbf2ee7
NguyenQuangBinhSteve <binhsteve123@gmail.com>
2025-08-13 21:31:32 +0700

    cs

--- Controllers/CoffeeShopAdminController.cs
+++ Controllers/CoffeeShopAdminController.cs
@@ -1,34 +1,13 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using CoffeeShopWeb.Models; // cần để dùng Context
 
-namespace CoffeeShopWeb.Controllers
+[Authorize(Policy = "AdminOnly")]
+public class CoffeeShopAdminController : Controller
 {
-    [Authorize(Policy = "AdminOnly")]
-    public class CoffeeShopAdminController : Controller
+    public IActionResult Index()
     {
-        private readonly Context _context;
-
-        public CoffeeShopAdminController(Context context)
-        {
-            _context = context;
-        }
-
-        public async Task<IActionResult> Index()
-        {
-            // Lấy dữ liệu thống kê số sản phẩm mỗi Category
-            var categoryData = await _context.Categories
-                .Select(c => new
-                {
-                    CategoryName = c.Name,
-                    ProductCount = _context.Products.Count(p => p.CategoryId == c.Id)
-                })
-                .ToListAsync();
-
-            ViewBag.CategoryChartData = categoryData;
-
-            return View();
-        }
+        var adminName = User?.Identity?.Name ?? "Admin";
+        ViewBag.AdminName = adminName;
+        return View();
     }
 }
